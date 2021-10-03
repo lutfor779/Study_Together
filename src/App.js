c389295971a24700746359975fa08159bc23eae3
lutfor779 <mdlrrahman779@gmail.com>
2021-10-04 00:20:38 +0600

    header and footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-danger">hello</h1>
-    </div>
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          
+        </Route>
+        <Route path="/home">
+
+        </Route>
+        <Route path="/courses">
+
+        </Route>
+        <Route path="/features">
+
+        </Route>
+        <Route path="/services">
+
+        </Route>
+        <Route path="/about">
+
+        </Route>
+        <Route path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer className="text-center p-3 bg-dark text-white">
+            <small>Copyright ©2021 All rights reserved | This template is made by Lutfor Rahman</small>
+        </footer>
+    );
+};
+
+export default Footer;
